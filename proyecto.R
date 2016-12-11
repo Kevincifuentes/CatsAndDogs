ef8eb1189aa1df61a55635bbccee0d7bcbe3d806
kevincifuentes <kevin@192.168.0.11>
2016-12-11 12:46:01 +0100

    Canny Edge detection

--- proyecto.R
+++ proyecto.R
@@ -3,16 +3,18 @@ rm(list = ls())
 library(imager)
 library(stringr)
 # Set wd where images are located
-setwd("C:\\Users\\LAPTOP\\Desktop\\I.A.A\\PROYECTO\\train")
+#setwd("C:\\Users\\LAPTOP\\Desktop\\I.A.A\\PROYECTO\\train")
+setwd("/Users/kevin/Desktop/Master/InteligenciaArtificial/R/Proyecto/train")
 images <- list.files() # Load images names
-fpath<-"C:\\Users\\LAPTOP\\Desktop\\I.A.A\\PROYECTO\\train\\"
+#fpath<-"C:\\Users\\LAPTOP\\Desktop\\I.A.A\\PROYECTO\\train\\"
+fpath<-"/Users/kevin/Desktop/Master/InteligenciaArtificial/R/Proyecto/train/"
 
 #Variables para la dimension de la imagen
 height=300
 width=300
 
 images<-as.data.frame(images)
-images = images[-203,] #En la ultima fila añade Thumbs ??, lo borro, hay q mirar por q lo añade
+images = images[-203,] #En la ultima fila a?ade Thumbs ??, lo borro, hay q mirar por q lo a?ade
 
 for(i in 1:length(images)){ 
   #Bucle para pasar por todas las imagenes de la lista
@@ -27,7 +29,7 @@ for(i in 1:length(images)){
 ####### UTILIZAMOS LA LIBRERIA IMAGER ########################################################
 
 library(imager)
-pet <- load.image(str_c(fpath,images[9])) # leer con libreria imager
+pet <- load.image(str_c(fpath,images[10])) # leer con libreria imager
 pet<- resize(pet,height,width)
 plot(pet,main="foto")
 #Redimensionamos la imagen
@@ -41,13 +43,13 @@ mean_color_image<-mean(pet$value)
 
 #Sacar valor Rojo promedio, sumar los primeros 40000 valores del canal 1 y sacar la media
 red<-head(pet,40000)
-mean_red<-mean(red$value)
+feature_mean_red<-mean(red$value)
 #Sacar valor Verde promedio, sumar los valores entre 40000 y 80000 del canal 2 y sacar la media
 green<-subset(pet,cc==2)
-mean_green<-mean(green$value)
+feature_mean_green<-mean(green$value)
 #Sacar valor Azul promedio, sumar los ultimos 40000 valores del canal 3 y sacar la media
 blue<-tail(pet,40000)
-mean_blue<-mean(blue$value)
+feature_mean_blue<-mean(blue$value)
 
 #Sacar la luminosidad
 pet <- load.image(str_c(fpath,images[10])) # leer con libreria imager  58
@@ -56,7 +58,7 @@ pet<- resize(pet,height,width)
 grayimage=grayscale(pet)
 plot(grayscale(pet))
 pet<- as.data.frame(pet)
-mean_color_grayimage<-mean(pet$value)
+feature_mean_color_grayimage<-mean(pet$value)
 #La media nos da lo mismo que con la imagen en color, PREGUNTAR PORQUE
 
 ###########  Image Gradient ####################################################
@@ -68,22 +70,22 @@ gradient<-as.data.frame(gradient)
 
 #Sacar el valor promedio del gradiente, todos los canales
 subset_gradient<-subset(gradient,gradient$value>=0)
-mean_gradient<-mean(subset_gradient$value)
+feature_mean_gradient<-mean(subset_gradient$value)
 
 #Sacar el valor promedio del gradiente en el canal 1 (red)
 subset_gradient_channel1<-subset(subset_gradient, subset_gradient$cc==1)
-mean_gradient_channel1<-mean(subset_gradient_channel1$value)
+feature_mean_gradient_channel1<-mean(subset_gradient_channel1$value)
 #Sacar el valor promedio del gradiente en el canal 2 (green)
 subset_gradient_channel2<-subset(subset_gradient, subset_gradient$cc==2)
-mean_gradient_channel2<-mean(subset_gradient_channel2$value)
+feature_mean_gradient_channel2<-mean(subset_gradient_channel2$value)
 #Sacar el valor promedio del gradiente en el canal 3 (blue)
 subset_gradient_channel3<-subset(subset_gradient, subset_gradient$cc==3)
-mean_gradient_channel3<-mean(subset_gradient_channel3$value)
+feature_mean_gradient_channel3<-mean(subset_gradient_channel3$value)
 
 
 ################## Determinant of Gesian ###################################################
 
-#Detectar objetos pequeños
+#Detectar objetos cuantos objectos peque?os
 get.centers <- function(im,thr="99%"){
   dt <- imhessian(im) %$% { xx*yy - xy^2 } %>% threshold(thr) %>% label
   as.data.frame(dt) %>% subset(value>0) %>% dplyr::group_by(value) %>% dplyr::summarise(mx=mean(x),my=mean(y))
@@ -91,6 +93,8 @@ get.centers <- function(im,thr="99%"){
 
 plot(grayimage)
 get.centers(grayimage,"99%") %$% points(mx,my,col="red")
+feature_numberOfLittleObjects = dim(get.centers(grayimage,"99%"))[1]
+
 
 #Detectar objetos de diferente escala
 #Compute determinant at scale "scale". 
@@ -98,16 +102,109 @@ hessdet <- function(im,scale=1) isoblur(im,scale) %>% imhessian %$% { scale^2*(x
 #Note the scaling (scale^2) factor in the determinant
 result_scale=as.data.frame(hessdet(grayimage,1))
 plot(hessdet(grayimage,1),main="Determinant of the Hessian at scale 1")
-get.centers(grayimage,"98.5%") %$% points(mx,my,col="red")
+get.centers(grayimage,"95.5%") %$% points(mx,my,col="red")
+feature_numberOfMediumObjects = dim(get.centers(grayimage,"95.5%"))[1]
 
 prueba<-as.data.frame(get.centers(grayimage,"98.5%"))
 
+################## Canny Edge ###################################################
+#funciones alternativas para el mismo uso >>>>>>>>> http://dahtah.github.io/imager/canny.html
 
+pet <- load.image(str_c(fpath,images[10]))
 
+#PASO1: denoising
+im <- grayscale(pet) %>% isoblur(2)
+plot(im)
 
+#PASO2: image gradient, magnitude and angle
+gr <- imgradient(im,"xy")
+plot(gr,layout="row")
 
+mag <- with(gr,sqrt(x^2+y^2))
+plot(mag)
 
+ang <- with(gr,atan2(y,x))
+plot(ang)
 
+#PASO 3: Cleaning using non-maxima suppression
+threshold(mag) %>% plot
+
+#Going along the (normalised) gradient
+#Xc(im) is an image containing the x coordinates of the image
+nX <- Xc(im) + gr$x/mag 
+nY <- Yc(im) + gr$y/mag
+#nX and nY are not integer values, so we can't use them directly as indices.
+#We can use interpolation, though:
+val.fwd <- interp(mag,data.frame(x=as.vector(nX),y=as.vector(nY)))
+
+nX <- Xc(im) - gr$x/mag 
+nY <- Yc(im) - gr$y/mag
+val.bwd <- interp(mag,data.frame(x=as.vector(nX),y=as.vector(nY)))
   
   
+#CONSEGUIMOS NEUTRALIZAR LOS NO MAXIMOS, COMPROBANDO SUS DOS VECINOS A CADA LADO
+throw <- (mag < val.bwd) | (mag < val.fwd)
+mag[throw] <- 0
+plot(mag)
 
+#PASO4: Hysteresis
+#Detectamos los bordes mediante t1 y t2
+#Las magnitudes por encima de t1 son bordes fuertes (prominentes)
+#Las magnitudes por encima de t2 son bordes debiles (suaves)
+#strong threshold
+t2 <- quantile(mag,.96)
+#weak threshold 
+t1 <- quantile(mag,.90)
+layout(t(1:2))
+
+strong <- as.cimg(mag>t2)
+plot(strong,main="Initial set of strong edges")
+weak <- as.cimg(mag %inr% c(t1,t2))
+plot(weak,main="Initial set of weak edges")
+
+#El proceso de Hysteresis normal se haria mediante un bÃºcle, comparando vecinos
+#pero no es optimo para R. UtilizarÃ© otro enfoque que han comentado: Morphological Dilatation
+overlap <- dilate_square(strong,3)*weak 
+strong.new <- strong + overlap
+plot(strong.new,main="New set of strong edges")
+
+delta <- sum(strong.new)-sum(strong)
+#Numero de bordes prominentes detectados en la imagen
+feature_detectedStrongEdges = delta
+
+#Permite realizar una iteracion hasta llegar a un punto en el que el valor no cambia
+#NECESARIO PARA EL PROCESO
+library(fixedpoints)
+
+#ws is a list containing two fields, "weak" and "strong"
+#which are images where all pixels with value 1 are "weak edges" (resp. strong)
+#the function expands the set of strong pixels via dilation and overlap
+#and returns the expanded strong set and the (shrunk) weak set
+expandStrong <- function(ws)
+{
+  overlap <- dilate_square(ws$strong,3)*ws$weak
+  ws$strong <- ws$strong + overlap
+  ws$weak <- ws$weak-overlap
+  ws
+}
+
+
+#hystFP is a new function that will call expandStrong repeatedly until
+#the weak and strong sets don't change anymore
+hystFP <- fp(expandStrong)
+
+#Call hystFP
+out <- list(strong=strong,weak=weak) %>% hystFP
+out
+
+canny <- out$strong
+plot(canny,main="Canny edges")
+
+lab <- label(strong,TRUE)*strong
+loc <- as.data.frame(lab) %>% dplyr::filter(value > 0)%>%
+dplyr::group_by(value) %>%
+dplyr::summarize(x=x[1],y=y[1])
+plot(strong)
+points(loc$x,loc$y,col="red")
+
+feature_numberOfStringPixelsForEdges = dim(loc)[1]
